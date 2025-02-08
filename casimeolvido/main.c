9b81ee1028d5d267cade6ba805b9e0b014ef6912
Frandevep <frandevep@gmail.com>
2025-02-07 23:38:44 -0300

    Add files via upload

---
+++ casimeolvido/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hola\n");
+    printf("casi\n");
+    printf("me\n");
+    printf("olvido\n");
+    printf("de\n");
+    printf("subir\n");
+    printf("codigo\n");
+
+    return 0;
+}
